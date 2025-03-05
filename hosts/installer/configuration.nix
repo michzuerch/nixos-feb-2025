dab002ec9ac602f2982b3b983a99651f9f79ed1a
Michael Zuercher <michzuerch@gmail.com>
2025-03-05 16:37:31 +0100

    change base of installer image to plasma6

--- hosts/installer/configuration.nix
+++ hosts/installer/configuration.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   imports = [
-    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
   ];
 
   nixpkgs.config.pulseaudio = true;
@@ -22,9 +22,42 @@
     neovim
     disko
     parted
-    git
     gh
   ];
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    useXkbConfig = false;
+  };
+
+  programs = {
+    nm-applet.enable = true;
+    zsh.enable = true;
+    git.enable = true;
+    dconf.enable = true;
+    light.enable = true;
+    mtr.enable = true;
+  };
+
+  time.timeZone = "Europe/Busingen";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+
+
+  networking.networkmanager.enable = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nixpkgs.hostPlatform = "x86_64-linux";
