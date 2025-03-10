a3a736351c47137dad66236fd4eea8ee9fbfd6c8
Michael Zuercher <michzuerch@gmail.com>
2025-03-10 17:49:44 +0100

    Add binary caches

--- hosts/ThinkpadNomad/modules/cachix.nix
+++ hosts/ThinkpadNomad/modules/cachix.nix
@@ -2,4 +2,17 @@
   environment.systemPackages = with pkgs; [
     cachix
   ];
+  nix.settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://colmena.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
