63f8367e32b9c23de19be87847959cb512cb497e
Michael Zuercher <michzuerch@gmail.com>
2025-03-10 17:54:34 +0100

    add key for cachix server

--- hosts/VM/modules/cachix.nix
+++ hosts/VM/modules/cachix.nix
@@ -13,6 +13,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
