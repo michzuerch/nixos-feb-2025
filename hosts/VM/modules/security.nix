fc81028799516cd7686563f7f1cfcc52a8d94cf8
Michael Zuercher <michzuerch@gmail.com>
2025-03-13 11:58:06 +0100

    more security

--- hosts/VM/modules/security.nix
+++ hosts/VM/modules/security.nix
@@ -8,4 +8,44 @@
     #clamav       #scan command: sudo freshclam; clamscan [options] [file/directory/-]
     chkrootkit #scan command: sudo chkrootkit
   ];
+  security = {
+    rtkit.enable = true;
+    polkit.enable = true;
+    tpm2 = {
+      enable = true;
+      pkcs11.enable = true;
+      tctiEnvironment.enable = true;
+    };
+    pam.services.hyprlock.text = "auth include login";
+    sudo = {
+      enable = true;
+      extraRules = [
+        {
+          commands = [
+            {
+              command = "${pkgs.systemd}/bin/systemctl suspend";
+              options = ["NOPASSWD"];
+            }
+            {
+              command = "${pkgs.systemd}/bin/reboot";
+              options = ["NOPASSWD"];
+            }
+            {
+              command = "${pkgs.systemd}/bin/poweroff";
+              options = ["NOPASSWD"];
+            }
+            {
+              command = "/run/current-system/sw/bin/nixos-rebuild";
+              options = ["NOPASSWD"];
+            }
+            {
+              command = "${pkgs.systemd}/bin/systemctl";
+              options = ["NOPASSWD"];
+            }
+          ];
+          groups = ["wheel"];
+        }
+      ];
+    };
+  };
 }
