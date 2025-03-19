31a66e69f33b3354212b83b5dd0a3ab74a37e17d
Michael Zuercher <michzuerch@gmail.com>
2025-03-19 17:08:35 +0100

    Ready for first install

--- hosts/ThinkpadNomad/disko.nix
+++ hosts/ThinkpadNomad/disko.nix
@@ -7,14 +7,17 @@
         content = {
           type = "gpt";
           partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
+              size = "512M";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
               };
             };
             plainSwap = {
