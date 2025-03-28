3cb976dab73f7db1ee1b4d42ebdee47d24325688
Michael Zuercher <michzuerch@gmail.com>
2025-03-28 16:51:59 +0100

    smaller and better VM

--- hosts/VM/modules/default.nix
+++ hosts/VM/modules/default.nix
@@ -1,8 +1,8 @@
 {
   imports = [
-    ../../common/modules/audio.nix
-    ../../common/modules/bluetooth.nix
-    ../../common/modules/documentation.nix
+    #../../common/modules/audio.nix
+    #../../common/modules/bluetooth.nix
+    #../../common/modules/documentation.nix
     ../../common/modules/displaymanager.nix
     ../../common/modules/security.nix
     ../../common/modules/environment-variables.nix
@@ -10,10 +10,10 @@
     ../../common/modules/fwupd.nix
     ../../common/modules/gc.nix
     ../../common/modules/i18n.nix
-    ../../common/modules/plasma.nix
+    #../../common/modules/plasma.nix
     ../../common/modules/hyprland.nix
-    ../../common/modules/graphics.nix
-    ../../common/modules/hacking.nix
+    #../../common/modules/graphics.nix
+    #../../common/modules/hacking.nix
     ../../common/modules/info-fetchers.nix
     ../../common/modules/kernel.nix
     #../../common/modules/networking.nix
@@ -22,11 +22,11 @@
     ../../common/modules/nh.nix
     ../../common/modules/nix-settings.nix
     ../../common/modules/nix-tools.nix
-    ../../common/modules/powermanagement.nix
-    ../../common/modules/ollama.nix
+    #../../common/modules/powermanagement.nix
+    #../../common/modules/ollama.nix
     #../../common/modules/rust.nix
     ../../common/modules/rust-fenix.nix
-    ../../common/modules/virtualisation.nix
+    #../../common/modules/virtualisation.nix
     ../../common/modules/xdg.nix
   ];
 }
